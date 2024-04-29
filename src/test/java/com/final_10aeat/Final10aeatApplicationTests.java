fa6118ed150654d7a81c704aa5a490af936230a3
최혜미 <ghrltjdtprbs@gmail.com>
2024-04-29 16:09:43 +0900

    [fix] 프로젝트 재등록

---
+++ src/test/java/com/final_10aeat/Final10aeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_10aeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final10aeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
