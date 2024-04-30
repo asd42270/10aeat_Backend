8f09971ad8bd4031caf54a78ed8ef294746d468d
hym <ghrltjdtprbs@gmail.com>
2024-04-30 10:27:15 +0900

    [feat] ResponseDTO 클래스 추가
    변경 사항:
    - ResponseDTO 클래스 추가. 이 클래스는 HTTP 응답을 나타내며, 코드를 간단하고 명확하게 작성할 수 있게 도와줍니다.
    - 일반적인 HTTP 응답을 생성하기 위한 여러 메서드가 제공됩니다: - `ok()`: 아무런 메시지, 데이터 없이 성공임을 알립니다.
      - `okWithData(T data)`: 데이터가 포함된 성공적인 응답을 반환합니다.
      - `okWithDataAndMessage(String message, T data)`: 메시지와 데이터가 모두 포함된 성공적인 응답을 반환합니다.
      - `error(ErrorCode errorCode)`: RestAdvice에서 applicationException을 처리할 때 사용합니다.
      - `errorWithMessage(HttpStatus httpStatus, String errorMessage)`: RestAdvice에서 applicationException 외의 모든 예외를 처리할 때 사용합니다.

---
+++ src/main/java/com/final_10aeat/global/util/ResponseDTO.java
@@ -0,0 +1,61 @@
+package com.final_10aeat.global.util;
+
+import com.final_10aeat.global.exception.ErrorCode;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ResponseDTO<T> {
+
+    private final int code;
+    private final T data;
+    private final String message;
+
+    @Builder
+    private ResponseDTO(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static ResponseDTO<Void> ok() {
+        return ResponseDTO.<Void>builder()
+            .code(HttpStatus.OK.value())
+            .data(null)
+            .message(null)
+            .build();
+    }
+
+    public static <T> ResponseDTO<T> okWithData(T data) {
+        return ResponseDTO.<T>builder()
+            .code(HttpStatus.OK.value())
+            .data(data)
+            .message(null)
+            .build();
+    }
+
+    public static <T> ResponseDTO<T> okWithDataAndMessage(String message, T data) {
+        return ResponseDTO.<T>builder()
+            .code(HttpStatus.OK.value())
+            .message(message)
+            .data(data)
+            .build();
+    }
+
+    public static ResponseDTO<Void> error(ErrorCode errorCode) {
+        return ResponseDTO.<Void>builder()
+            .code(errorCode.getHttpStatus().value())
+            .message(errorCode.getMessage())
+            .data(null)
+            .build();
+    }
+
+    public static ResponseDTO<Void> errorWithMessage(HttpStatus httpStatus, String errorMessage) {
+        return ResponseDTO.<Void>builder()
+            .code(httpStatus.value())
+            .message(errorMessage)
+            .data(null)
+            .build();
+    }
+}
