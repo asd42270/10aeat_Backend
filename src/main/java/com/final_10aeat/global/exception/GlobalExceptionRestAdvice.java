955aa98919317e11774eadf86e417417a598f126
hym <ghrltjdtprbs@gmail.com>
2024-04-30 10:28:48 +0900

    [feat] 공통 예외 처리 클래스 추가
    변경 사항:
    - `ApplicationException` 클래스 추가: 비즈니스 로직 실패에 대한 예외 처리를 위해 만들어진 클래스로, 이를 상속하여 다양한 예외를 정의할 수 있습니다.
    - `ErrorCode` 열거형 추가: HTTP 상태 코드와 함께 해당 예외에 대한 메시지를 제공합니다.
    - `GlobalExceptionRestAdvice` 클래스 추가: `@RestControllerAdvice`를 통해 다양한 예외를 처리하는 글로벌 예외 처리 클래스로, 아래와 같은 메서드들을 포함합니다:
      - `applicationException()`: `ApplicationException`을 처리하고, 적절한 HTTP 상태 코드와 메시지로 응답을 생성합니다.
      - `bindException()`: 데이터 바인딩 중 발생하는 `BindException`을 처리하고, 400 Bad Request 응답을 생성합니다.
      - `dbException()`: `DataAccessException`을 처리하고, 500 Internal Server Error 응답을 생성합니다.
      - `serverException()`: `RuntimeException`과 같은 서버 관련 예외를 처리하고, 500 Internal Server Error 응답을 생성합니다.

---
+++ src/main/java/com/final_10aeat/global/exception/GlobalExceptionRestAdvice.java
@@ -0,0 +1,48 @@
+package com.final_10aeat.global.exception;
+
+import com.final_10aeat.global.util.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionRestAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<ResponseDTO<Void>> applicationException(ApplicationException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity
+            .status(e.getErrorCode().getHttpStatus())
+            .body(ResponseDTO.error(e.getErrorCode()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ResponseDTO<Void>> bindException(BindException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ResponseDTO.errorWithMessage(HttpStatus.BAD_REQUEST,
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ResponseDTO<Void>> dbException(DataAccessException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ResponseDTO.errorWithMessage(HttpStatus.INTERNAL_SERVER_ERROR, "db 에러!"));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ResponseDTO<Void>> serverException(RuntimeException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ResponseDTO.errorWithMessage(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러!"));
+    }
+}
