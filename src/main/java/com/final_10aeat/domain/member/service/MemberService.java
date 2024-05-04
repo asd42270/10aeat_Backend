07930e3267756ac7d0ebcf8201340bd57a91162f
이유빈 <lyb991113@gmail.com>
2024-05-04 21:41:52 +0900

    [fix] MemberService 수정 - @Transactional 추가 - login메서드에 @Transactional(readOnly = true) 추가 - MemberRequestDto-> MemberLoginRequestDto로 변경되어 수정

--- src/main/java/com/final_10aeat/domain/member/service/MemberService.java
+++ src/main/java/com/final_10aeat/domain/member/service/MemberService.java
@@ -1,8 +1,8 @@
 package com.final_10aeat.domain.member.service;
 
-import com.final_10aeat.domain.member.entity.Member;
+import com.final_10aeat.domain.member.dto.request.MemberLoginRequestDto;
 import com.final_10aeat.domain.member.dto.request.MemberRegisterRequestDto;
-import com.final_10aeat.domain.member.dto.request.MemberRequestDto;
+import com.final_10aeat.domain.member.entity.Member;
 import com.final_10aeat.domain.member.exception.MemberDuplicatedException;
 import com.final_10aeat.domain.member.exception.MemberNotExistException;
 import com.final_10aeat.domain.member.repository.MemberRepository;
@@ -10,9 +10,11 @@ import com.final_10aeat.global.security.jwt.JwtTokenGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -21,26 +23,28 @@ public class MemberService {
 
     //TODO: 응답 값에 대한 부분은 프론트와 논의 필요
     public void register(MemberRegisterRequestDto request) {
-        //1. 유효성 검사
+        // 1. 유효성 검사
         if (memberRepository.existsByEmail(request.email())) {
             throw new MemberDuplicatedException();
         }
 
-        //2. 비밀번호 인코딩
+        // 2. 비밀번호 인코딩
         String password = passwordEncoder.encode(request.password());
 
-        //3. member로 변환
+        // 3. member로 변환
         Member member = Member.builder()
                 .email(request.email())
                 .password(password)
                 .build();
 
-        //4. save
+        // 4. save
         memberRepository.save(member);
     }
 
-    public String login(MemberRequestDto request) {
-        Member member = memberRepository.loadUserByUsername(request.email());
+    @Transactional(readOnly = true)
+    public String login(MemberLoginRequestDto request) {
+        Member member = memberRepository.findByEmail(request.email())
+                .orElseThrow(MemberNotExistException::new);
 
         if (!passwordMatcher(request.password(), member)) {
             throw new MemberNotExistException();
@@ -52,6 +56,4 @@ public class MemberService {
     private Boolean passwordMatcher(final String requestPassword, final Member member) {
         return passwordEncoder.matches(requestPassword, member.getPassword());
     }
-
-
 }
