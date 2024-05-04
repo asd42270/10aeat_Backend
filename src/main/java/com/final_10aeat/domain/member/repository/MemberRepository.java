e6fee14c006bc64bbcf11d0017269366ba1e3a5f
이유빈 <lyb991113@gmail.com>
2024-05-04 21:40:34 +0900

    [fix] MemberRepository 수정 - loadUserByUsername 메소드 삭제

--- src/main/java/com/final_10aeat/domain/member/repository/MemberRepository.java
+++ src/main/java/com/final_10aeat/domain/member/repository/MemberRepository.java
@@ -1,6 +1,5 @@
 package com.final_10aeat.domain.member.repository;
 
-
 import com.final_10aeat.domain.member.entity.Member;
 import com.final_10aeat.domain.member.exception.MemberNotExistException;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,9 +8,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Boolean existsByEmail(String email);
-    Optional<Member> findByEmail(String email);
 
-    default Member loadUserByUsername(String email){
-        return findByEmail(email).orElseThrow(MemberNotExistException::new);
-    }
+    Optional<Member> findByEmail(String email);
 }
