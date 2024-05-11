f66bc68f536ee1355dcfb2f3cc05f4fe3c39f9f1
hym <ghrltjdtprbs@gmail.com>
2024-05-12 02:09:27 +0900

    [feat] ManageArticle 관련 entity 추가
    변경 사항 :
     - ManageArticle,ManageSchedule 엔터티 추가했습니다.
     - ManagePeriod 는 enum으로 분리했습니다.

---
+++ src/main/java/com/final_10aeat/domain/manageArticle/entity/ManageSchedule.java
@@ -0,0 +1,41 @@
+package com.final_10aeat.domain.manageArticle.entity;
+
+import com.final_10aeat.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "manage_schedule")
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ManageSchedule extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ischeck")
+    private boolean isCheck;
+
+    @Column
+    private LocalDateTime schedule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manage_article_id")
+    private ManageArticle manageArticle;
+}
