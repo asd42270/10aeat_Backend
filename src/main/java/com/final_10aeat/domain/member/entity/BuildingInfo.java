ede6a9842f40b1a55b63eb09d3abe7b20ebdb8f6
hym <ghrltjdtprbs@gmail.com>
2024-05-12 00:39:26 +0900

    [feat] member관련 entity 추가
    변경 사항 :
     - Admin, BuildingInfo, Member, Office 엔터티 추가
     - MemberRole enum 클래스 추가
     - 다대다 관계는 전부 중간 테이블을 생성했습니다.

---
+++ src/main/java/com/final_10aeat/domain/member/entity/BuildingInfo.java
@@ -0,0 +1,41 @@
+package com.final_10aeat.domain.member.entity;
+
+import com.final_10aeat.domain.admin.entity.Office;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "member_building_info")
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BuildingInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private int dong;
+
+    @Column
+    private int ho;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "office_id")
+    private Office office;
+
+}
--- src/main/java/com/final_10aeat/domain/member/entity/Member.java
+++ src/main/java/com/final_10aeat/domain/member/entity/Member.java
@@ -1,15 +1,15 @@
 package com.final_10aeat.domain.member.entity;
 
+import com.final_10aeat.domain.admin.entity.Office;
 import com.final_10aeat.global.entity.SoftDeletableBaseTimeEntity;
 import jakarta.persistence.*;
+import java.util.Set;
 import lombok.*;
 
 @Entity
 @Getter
 @Builder
-@Table(name = "member", indexes = @Index(
-        name = "idx_email_deletedAt", columnList = "email, deletedAt", unique = true
-))
+@Table(name = "member")
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends SoftDeletableBaseTimeEntity {
@@ -23,4 +23,27 @@ public class Member extends SoftDeletableBaseTimeEntity {
 
     @Column
     private String password;
+
+    @Column
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MemberRole role;
+
+    @ManyToMany
+    @JoinTable(
+        name = "member_building",
+        joinColumns = @JoinColumn(name = "member_id"),
+        inverseJoinColumns = @JoinColumn(name = "building_info_id")
+    )
+    private Set<BuildingInfo> buildingInfos;
+
+    @ManyToMany
+    @JoinTable(
+        name = "member_office",
+        joinColumns = @JoinColumn(name = "member_id"),
+        inverseJoinColumns = @JoinColumn(name = "office_id")
+    )
+    private Set<Office> offices;
 }
