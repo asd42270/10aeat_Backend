41a6f26f6d2d113fc148fa1e9610ef7383f0a0e1
hym <ghrltjdtprbs@gmail.com>
2024-05-12 01:15:18 +0900

    [feat] RepairArticle 관련 entity 추가
    변경 사항 :
     - RepairArticle, RepairArticleImage 엔터티를 추가했습니다.
     - 조회 기능만 필요한 Progress, ArticleCategory는 enum클래스로 분리했습니다.

---
+++ src/main/java/com/final_10aeat/domain/repairArticle/entity/RepairArticle.java
@@ -0,0 +1,70 @@
+package com.final_10aeat.domain.repairArticle.entity;
+
+import com.final_10aeat.domain.admin.entity.Admin;
+import com.final_10aeat.global.entity.SoftDeletableBaseTimeEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Set;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "repair_article")
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RepairArticle extends SoftDeletableBaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Progress progress = Progress.INPROGRESS;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ArticleCategory category;
+
+    @Column
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "construction_start")
+    private LocalDateTime startConstruction;
+
+    @Column(name = "construction_end")
+    private LocalDateTime endConstruction;
+
+    @Column(name = "repair_company")
+    private String company;
+
+    @Column(name = "repair_company_website", columnDefinition = "TEXT")
+    private String companyWebsite;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "admin_id")
+    private Admin admin;
+
+    @OneToMany(mappedBy = "repairArticle", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<RepairArticleImage> images;
+}
---
+++ src/main/java/com/final_10aeat/domain/repairArticle/entity/RepairArticleImage.java
@@ -0,0 +1,36 @@
+package com.final_10aeat.domain.repairArticle.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "repair_image")
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RepairArticleImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", name = "url")
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private RepairArticle repairArticle;
+}
