267a2d25df4d495fbae79f2e251b939d66219241
Galmaeki <kjh09800@gmail.com>
2024-05-18 14:05:06 +0900

    [Fix] TokenGenerator getRole 메소드 추가
    - 추후 토큰에서 권한 확인을 위해 getRole 메소드 추가

--- src/main/java/com/final_10aeat/global/security/jwt/JwtTokenGenerator.java
+++ src/main/java/com/final_10aeat/global/security/jwt/JwtTokenGenerator.java
@@ -1,6 +1,7 @@
 package com.final_10aeat.global.security.jwt;
 
 
+import com.final_10aeat.domain.member.entity.MemberRole;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
@@ -29,9 +30,10 @@ public class JwtTokenGenerator {
     private final Long accessExpiredTimeMills = 1000 * 60 * 30L;
 
 
-    public String createJwtToken(String email) {
+    public String createJwtToken(String email, MemberRole memberRole) {
         return Jwts.builder()
                 .claim("email", email)
+                .claim("role",memberRole)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + accessExpiredTimeMills))
                 .signWith(key)
@@ -43,6 +45,10 @@ public class JwtTokenGenerator {
         return claims.get("email", String.class);
     }
 
+    public String getRole(String token) {
+        Claims claims = extractClaim(token);
+        return claims.get("role", String.class);
+    }
 
     private Claims extractClaim(String token) {
         return Jwts.parser()
