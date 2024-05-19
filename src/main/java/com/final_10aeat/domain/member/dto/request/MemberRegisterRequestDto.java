9113074a066aeaba6ab2bdc789a230b6097e6b54
이유빈 <lyb991113@gmail.com>
2024-05-20 01:31:55 +0900

    [Feat] 회원 가입을 구현한다. - MemberRegisterRequestDto: 동, 호수, MemberRole 추가했습니다. - MemberService: BuildingInfo의 Office에 대한 정보를 어떻게 저장하면 좋을 지에 대해서 논의가 필요할 것 같습니다. 이 부분을 우선 null로 처리하고 회원가입 로직 작성했습니다. - BuildingInfoRepository: 회원 가입 시 빌딩 정보 저장을 위해 추가했습니다.

--- src/main/java/com/final_10aeat/domain/member/dto/request/MemberRegisterRequestDto.java
+++ src/main/java/com/final_10aeat/domain/member/dto/request/MemberRegisterRequestDto.java
@@ -5,5 +5,7 @@ import com.final_10aeat.domain.member.entity.MemberRole;
 public record MemberRegisterRequestDto(
         String email,
         String password,
+        String dong,
+        String ho,
         MemberRole memberRole
 ) { }
--- src/main/java/com/final_10aeat/domain/member/service/MemberService.java
+++ src/main/java/com/final_10aeat/domain/member/service/MemberService.java
@@ -3,9 +3,11 @@ package com.final_10aeat.domain.member.service;
 import com.final_10aeat.domain.member.dto.request.MemberLoginRequestDto;
 import com.final_10aeat.domain.member.dto.request.MemberRegisterRequestDto;
 import com.final_10aeat.domain.member.dto.request.MemberWithdrawRequestDto;
+import com.final_10aeat.domain.member.entity.BuildingInfo;
 import com.final_10aeat.domain.member.entity.Member;
 import com.final_10aeat.domain.member.exception.MemberDuplicatedException;
 import com.final_10aeat.domain.member.exception.MemberNotExistException;
+import com.final_10aeat.domain.member.repository.BuildingInfoRepository;
 import com.final_10aeat.domain.member.repository.MemberRepository;
 import com.final_10aeat.global.security.jwt.JwtTokenGenerator;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenGenerator jwtTokenGenerator;
+    private final BuildingInfoRepository buildingInfoRepository;
 
     //TODO: 응답 값에 대한 부분은 프론트와 논의 필요
     public void register(MemberRegisterRequestDto request) {
@@ -33,11 +37,21 @@ public class MemberService {
         // 2. 비밀번호 인코딩
         String password = passwordEncoder.encode(request.password());
 
-        // 3. member로 변환
+        // 3. dong, ho를 BuildingInfo로 변환, member로 변환
+        BuildingInfo buildingInfo = BuildingInfo.builder()
+                .dong(request.dong())
+                .ho(request.ho())
+                .office(null)
+                .build();
+
+        //BuildingInfo를 먼저 저장해준다.
+        BuildingInfo savedBuildingInfo = buildingInfoRepository.save(buildingInfo);
+
         Member member = Member.builder()
                 .email(request.email())
                 .password(password)
                 .role(request.memberRole())
+                .buildingInfos(Set.of(savedBuildingInfo))
                 .build();
 
         // 4. save
