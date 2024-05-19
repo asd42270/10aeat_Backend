eb9cfb5c7a36dc09a402f590d7cc97c2a4bd4bc7
Galmaeki <kjh09800@gmail.com>
2024-05-19 12:22:48 +0900

    [fix] 코드 줄바꿈

--- src/main/java/com/final_10aeat/controller/HealthCheckController.java
+++ src/main/java/com/final_10aeat/controller/HealthCheckController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 @RequiredArgsConstructor
 public class HealthCheckController {
+
     private final StringRedisTemplate redisTemplate;
 
     @GetMapping("/server")
