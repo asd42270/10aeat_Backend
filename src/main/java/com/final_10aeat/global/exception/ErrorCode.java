9f52c00878dcdc7e816745795e899c16ccc35803
이유빈 <lyb991113@gmail.com>
2024-05-20 20:13:09 +0900

    [Feat] 회원가입, 탈퇴를 구현한다. - ErrorCode: 에러 코드를 추가한다.

--- src/main/java/com/final_10aeat/global/exception/ErrorCode.java
+++ src/main/java/com/final_10aeat/global/exception/ErrorCode.java
@@ -16,6 +16,7 @@ public enum ErrorCode {
     MEMBER_NOT_EXIST(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자입니다."),
     TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "토큰을 찾을 수 없습니다."),
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "중복된 이메일입니다."),
+    MEMBER_MISMATCH(HttpStatus.CONFLICT, "일치하지 않는 사용자입니다."),
 
     // 5xx
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러");
