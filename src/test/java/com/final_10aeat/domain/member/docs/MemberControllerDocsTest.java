b1cf150fbaef35b075676a005db3fdd354e8988e
galmaeki <kjh09800@gmail.com>
2024-05-20 14:22:38 +0900

    [Refactor] 테스트 코드 토큰 추가

--- src/test/java/com/final_10aeat/domain/member/docs/MemberControllerDocsTest.java
+++ src/test/java/com/final_10aeat/domain/member/docs/MemberControllerDocsTest.java
@@ -1,5 +1,8 @@
 package com.final_10aeat.domain.member.docs;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
@@ -52,7 +55,11 @@ public class MemberControllerDocsTest extends RestDocsSupport {
             "test@example.com", "password"
         );
 
-        // when & then
+        // when
+        when(memberService.login(loginRequest))
+            .thenReturn("token");
+
+        // then
         mockMvc.perform(RestDocumentationRequestBuilders.post("/members/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(loginRequest)))
@@ -64,6 +71,10 @@ public class MemberControllerDocsTest extends RestDocsSupport {
                     fieldWithPath("email").description("로그인 할 계정의 이메일"),
                     fieldWithPath("password").description("로그인 할 회원의 비밀번호")
                 ),
+                responseHeaders(
+                    headerWithName("accessToken").description("로그인 정보가 포함된 토큰")
+                )
+                ,
                 responseFields(
                     fieldWithPath("code").description("응답 상태 코드")
                 )
