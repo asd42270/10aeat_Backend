9aa70f0ce2837d8d5378a26b9f234cc003258cdc
이유빈 <lyb991113@gmail.com>
2024-05-20 20:59:18 +0900

    [Refactor] 테스트코드를 수정한다. - WithdrawServiceTest:  - MemberService의 어노테이션을 @InjectMocks로 변경  - setUp()에서 탈퇴 전 회원가입을 위한 메서드를 작성하고. 이를 위해 BuildingInfoRepository를 Mock으로 등록한다.

--- src/test/java/com/final_10aeat/domain/member/unit/WithdrawServiceTest.java
+++ src/test/java/com/final_10aeat/domain/member/unit/WithdrawServiceTest.java
@@ -2,29 +2,74 @@ package com.final_10aeat.domain.member.unit;
 
 import com.final_10aeat.domain.member.dto.request.MemberRegisterRequestDto;
 import com.final_10aeat.domain.member.dto.request.MemberWithdrawRequestDto;
+import com.final_10aeat.domain.member.entity.BuildingInfo;
+import com.final_10aeat.domain.member.entity.Member;
 import com.final_10aeat.domain.member.entity.MemberRole;
+import com.final_10aeat.domain.member.exception.MemberMissMatchException;
+import com.final_10aeat.domain.member.exception.MemberNotExistException;
+import com.final_10aeat.domain.member.repository.BuildingInfoRepository;
+import com.final_10aeat.domain.member.repository.MemberRepository;
 import com.final_10aeat.domain.member.service.MemberService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.mockito.Mockito.verify;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
 
 public class WithdrawServiceTest {
 
-    @Mock
+    @InjectMocks
     private MemberService memberService;
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private BuildingInfoRepository buildingInfoRepository;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
 
         String email = "spring";
-        String password= "spring";
-        MemberRegisterRequestDto requestDto = new MemberRegisterRequestDto(email, password, "103동", "2212호", MemberRole.TENANT);
+        String password = "spring";
+
+        BuildingInfo buildingInfo = BuildingInfo.builder()
+                .dong("102동")
+                .ho("2212호")
+                .office(null)
+                .build();
+
+        when(buildingInfoRepository.save(any(BuildingInfo.class))).thenReturn(buildingInfo);
+
+        Member member = Member.builder()
+                .id(1L)
+                .email(email)
+                .password(password)
+                .name("spring")
+                .role(MemberRole.TENANT)
+                .buildingInfos(Set.of(BuildingInfo.builder()
+                        .dong("102동")
+                        .ho("2212호")
+                        .office(null)
+                        .build()))
+                .build();
+
+        MemberRegisterRequestDto requestDto = new MemberRegisterRequestDto(email, password, "스프링", "103동", "2212호", MemberRole.TENANT);
+
         memberService.register(requestDto);
+
+        when(memberRepository.findByEmailAndDeletedAtIsNull(email)).thenReturn(Optional.of(member));
+        when(memberRepository.existsByEmailAndDeletedAtIsNull(email)).thenReturn(true);
+        when(passwordEncoder.matches(password, member.getPassword())).thenReturn(true);
     }
 
     @Test
@@ -36,6 +81,27 @@ public class WithdrawServiceTest {
 
         memberService.withdraw(memberRequest);
 
-        verify(memberService).withdraw(memberRequest);
+        verify(memberRepository).findByEmailAndDeletedAtIsNull(email);
+        verify(memberRepository).save(any(Member.class));
+    }
+
+    @Test
+    @DisplayName("다른 회원의 탈퇴를 시도한다.-사용자 불일치 오류 발생")
+    void _willMissMatch() {
+        String email = "spring";
+        String password = "asdasdasd";
+        MemberWithdrawRequestDto memberRequest = new MemberWithdrawRequestDto(email, password);
+
+        Assertions.assertThrows(MemberMissMatchException.class, () -> memberService.withdraw(memberRequest));
+    }
+
+    @Test
+    @DisplayName("회원 탈퇴하려는 계정이 존재하지 않는다.")
+    void _willNotExist() {
+        String email = "2222";
+        String password = "2222";
+        MemberWithdrawRequestDto memberRequest = new MemberWithdrawRequestDto(email, password);
+
+        Assertions.assertThrows(MemberNotExistException.class, () -> memberService.withdraw(memberRequest));
     }
 }
