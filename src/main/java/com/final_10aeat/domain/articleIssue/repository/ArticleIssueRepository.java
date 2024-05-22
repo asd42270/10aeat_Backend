7dc10ed1e22f68b8df100bdbfd78428b606e414b
이유빈 <lyb991113@gmail.com>
2024-05-22 18:50:40 +0900

    [Feature] 이슈 발행 기능을 구현한다. - RepairArticleRepository: 유지보수 게시글 조회를 위해 repository를 생성한다. - ManageArticleRepository: 유지관리 게시글 조회를 위해 repository를 생성한다. - ArticleIssueRepository: 이슈 repository를 생성한다. -

---
+++ src/main/java/com/final_10aeat/domain/articleIssue/repository/ArticleIssueRepository.java
@@ -0,0 +1,7 @@
+package com.final_10aeat.domain.articleIssue.repository;
+
+import com.final_10aeat.domain.articleIssue.entity.ArticleIssue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleIssueRepository extends JpaRepository<ArticleIssue, Long> {
+}
---
+++ src/main/java/com/final_10aeat/domain/repairArticle/repository/RepairArticleRepository.java
@@ -0,0 +1,7 @@
+package com.final_10aeat.domain.repairArticle.repository;
+
+import com.final_10aeat.domain.repairArticle.entity.RepairArticle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepairArticleRepository extends JpaRepository<RepairArticle, Long> {
+}
