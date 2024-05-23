11adf9c3d8492343401a283c11f05f544df45aff
hym <ghrltjdtprbs@gmail.com>
2024-05-23 15:47:01 +0900

    [test] DeleteRepairArticleSchedulerTest 추가
    변경 사항 :
     - DeleteRepairArticleScheduler 가 잘 동작하는지 테스트하기 위한 테스트 코드를 추가하였습니다.

--- src/test/java/com/final_10aeat/domain/repairArticle/unit/DeleteRepairArticleSchedulerTest.java
+++ src/test/java/com/final_10aeat/domain/repairArticle/unit/DeleteRepairArticleSchedulerTest.java
@@ -1,4 +1,4 @@
-package com.final_10aeat.domain.repairArticle.scheduler;
+package com.final_10aeat.domain.repairArticle.unit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.any;
