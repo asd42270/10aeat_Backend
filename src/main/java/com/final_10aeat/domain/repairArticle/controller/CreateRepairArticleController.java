fdf7a70dde6820320eb5a710dcd00fcd1b7c7bae
hym <ghrltjdtprbs@gmail.com>
2024-05-23 15:42:02 +0900

    [feat] 게시글 수정 controller 추가
    변경 사항 :
     - 게시글 수정 controller를 추가하였습니다.

--- src/main/java/com/final_10aeat/domain/repairArticle/controller/CreateRepairArticleController.java
+++ src/main/java/com/final_10aeat/domain/repairArticle/controller/CreateRepairArticleController.java
@@ -1,6 +1,7 @@
 package com.final_10aeat.domain.repairArticle.controller;
 
 import com.final_10aeat.domain.repairArticle.dto.request.CreateRepairArticleRequestDto;
+import com.final_10aeat.domain.repairArticle.dto.request.UpdateRepairArticleRequestDto;
 import com.final_10aeat.domain.repairArticle.service.RepairArticleService;
 import com.final_10aeat.global.security.principal.ManagerPrincipal;
 import com.final_10aeat.global.util.ResponseDTO;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,13 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('MANAGER')")
 @RequestMapping("/managers/repair/article")
-public class RepairArticleController {
+public class CreateRepairArticleController {
 
     private final RepairArticleService repairArticleService;
 
     @PostMapping
-    @PreAuthorize("hasRole('MANAGER')")
     public ResponseEntity<ResponseDTO<Void>> createRepairArticle(@RequestBody @Valid
     CreateRepairArticleRequestDto request,
         @AuthenticationPrincipal ManagerPrincipal managerPrincipal) {
@@ -33,11 +35,20 @@ public class RepairArticleController {
     }
 
     @DeleteMapping("/{repairArticleId}")
-    @PreAuthorize("hasRole('MANAGER')")
     public ResponseEntity<ResponseDTO<Void>> deleteRepairArticleById(
         @PathVariable Long repairArticleId,
         @AuthenticationPrincipal ManagerPrincipal managerPrincipal) {
         repairArticleService.deleteRepairArticleById(repairArticleId,
+            managerPrincipal.getManager().getId());
+        return ResponseEntity.ok(ResponseDTO.ok());
+    }
+
+    @PatchMapping("/{repairArticleId}")
+    public ResponseEntity<ResponseDTO<Void>> updateRepairArticleById(
+        @PathVariable Long repairArticleId,
+        @RequestBody @Valid UpdateRepairArticleRequestDto request,
+        @AuthenticationPrincipal ManagerPrincipal managerPrincipal) {
+        repairArticleService.updateRepairArticle(repairArticleId, request,
             managerPrincipal.getManager().getId());
         return ResponseEntity.ok(ResponseDTO.ok());
     }
