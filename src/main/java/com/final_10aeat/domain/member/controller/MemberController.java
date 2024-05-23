d4aa173ce001bb998cd69377100e909dcf551138
galmaeki <kjh09800@gmail.com>
2024-05-23 17:29:57 +0900

    [Fix] 로컬 확인용 코드 수정

--- src/main/java/com/final_10aeat/domain/member/controller/MemberController.java
+++ src/main/java/com/final_10aeat/domain/member/controller/MemberController.java
@@ -4,24 +4,27 @@ import com.final_10aeat.domain.member.dto.request.MemberLoginRequestDto;
 import com.final_10aeat.domain.member.dto.request.MemberRegisterRequestDto;
 import com.final_10aeat.domain.member.dto.request.MemberWithdrawRequestDto;
 import com.final_10aeat.domain.member.service.MemberService;
-import com.final_10aeat.global.security.principal.MemberPrincipal;
 import com.final_10aeat.global.util.ResponseDTO;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
+
     private final MemberService memberService;
 
     @PostMapping
     public ResponseDTO<Void> register(
-            HttpServletResponse response,
-            @RequestBody @Valid MemberRegisterRequestDto request
+        HttpServletResponse response,
+        @RequestBody @Valid MemberRegisterRequestDto request
     ) {
         MemberLoginRequestDto loginDto = memberService.register(request);
 
@@ -33,8 +36,8 @@ public class MemberController {
 
     @PostMapping("/login")
     public ResponseDTO<Void> login(
-            HttpServletResponse response,
-            @RequestBody @Valid MemberLoginRequestDto request) {
+        HttpServletResponse response,
+        @RequestBody @Valid MemberLoginRequestDto request) {
         String token = memberService.login(request);
         response.setHeader("accessToken", token);
         return ResponseDTO.ok();
@@ -42,16 +45,9 @@ public class MemberController {
 
     @DeleteMapping
     public ResponseDTO<Void> withdraw(
-            @RequestBody @Valid MemberWithdrawRequestDto request
-    ){
+        @RequestBody @Valid MemberWithdrawRequestDto request
+    ) {
         memberService.withdraw(request);
         return ResponseDTO.ok();
-    }
-
-    @GetMapping
-    public ResponseDTO<?> test(
-        @AuthenticationPrincipal MemberPrincipal principal
-    ){
-        return ResponseDTO.okWithData(principal.getMember());
     }
 }
