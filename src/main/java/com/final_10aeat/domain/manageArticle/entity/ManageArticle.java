8754326b47d6130ae41e86d65382ed3a3ac205e8
Galmaeki <kjh09800@gmail.com>
2024-05-25 14:58:07 +0900

    [feat] ManagerArticle 필드 수정
    문제 사항 :
    - 일부 어색한 부분 수정
    
    변경 사항 :
    - progress 기본 값 삭제
    - manager -> responsibility
    - int -> Integer
    - office 연관관계 추가

--- src/main/java/com/final_10aeat/domain/manageArticle/entity/ManageArticle.java
+++ src/main/java/com/final_10aeat/domain/manageArticle/entity/ManageArticle.java
@@ -2,6 +2,7 @@ package com.final_10aeat.domain.manageArticle.entity;
 
 import com.final_10aeat.common.enumclass.ManagePeriod;
 import com.final_10aeat.common.enumclass.Progress;
+import com.final_10aeat.domain.office.entity.Office;
 import com.final_10aeat.global.entity.SoftDeletableBaseTimeEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -12,6 +13,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
@@ -35,14 +38,14 @@ public class ManageArticle extends SoftDeletableBaseTimeEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Progress progress = Progress.INPROGRESS;
+    private Progress progress;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ManagePeriod period;
 
     @Column(name = "period_count")
-    private int periodCount;
+    private Integer periodCount;
 
     @Column
     private String title;
@@ -54,11 +57,15 @@ public class ManageArticle extends SoftDeletableBaseTimeEntity {
     private String target;
 
     @Column
-    private String manager;
+    private String responsibility;
 
     @Column(columnDefinition = "TEXT")
     private String note;
 
     @OneToMany(mappedBy = "manageArticle", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<ManageSchedule> schedules;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "office_id")
+    private Office office;
 }
