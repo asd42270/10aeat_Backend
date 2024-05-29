3408d0f14176683c3ba61cb2a885c75adacaba5b
hym <ghrltjdtprbs@gmail.com>
2024-05-29 12:11:29 +0900

    [refactor] UserIdAndRole 확장
    문제 사항 :
     - comment 도메인에서 사용했던 UserIdAndRole를 확장할 필요성 (다른 도메인에서도 사용할 수 있도록)
    
    변경 사항 :
     - UserIdAndRole common 패키지로 이동
     - 확장을 위핸 AuthenticationService 생성 (member인지 manager인지 구분, role 관련 없이 officeid 뽑아냄)

---
+++ src/main/java/com/final_10aeat/common/service/AuthenticationService.java
@@ -0,0 +1,31 @@
+package com.final_10aeat.common.service;
+
+import com.final_10aeat.common.dto.UserIdAndRole;
+import com.final_10aeat.global.security.principal.ManagerPrincipal;
+import com.final_10aeat.global.security.principal.MemberPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    public UserIdAndRole getCurrentUserIdAndRole() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof MemberPrincipal) {
+            return new UserIdAndRole(((MemberPrincipal) principal).getMember().getId(), false);
+        } else if (principal instanceof ManagerPrincipal) {
+            return new UserIdAndRole(((ManagerPrincipal) principal).getManager().getId(), true);
+        }
+        throw new IllegalStateException("Current user is not recognized as Member or Manager");
+    }
+
+    public Long getUserOfficeId() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof MemberPrincipal) {
+            return ((MemberPrincipal) principal).getMember().getDefaultOffice();
+        } else if (principal instanceof ManagerPrincipal) {
+            return ((ManagerPrincipal) principal).getManager().getOffice().getId();
+        }
+        throw new IllegalStateException("Current user does not have an associated office");
+    }
+}
