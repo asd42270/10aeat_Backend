a19c38cac7a42dfb910e3f711fa71d8b758a114d
galmaeki <kjh09800@gmail.com>
2024-05-30 13:54:40 +0900

    [fix] 사용하지 않는 import 문 삭제
    변경 사항 :
    - 테스트시 사용된 의존성 삭제

--- src/main/java/com/final_10aeat/common/scheduler/DeleteManageArticleScheduler.java
+++ src/main/java/com/final_10aeat/common/scheduler/DeleteManageArticleScheduler.java
@@ -1,21 +1,12 @@
 package com.final_10aeat.common.scheduler;
 
-import com.final_10aeat.common.enumclass.ManagePeriod;
-import com.final_10aeat.common.enumclass.Progress;
 import com.final_10aeat.domain.manageArticle.entity.ManageArticle;
 import com.final_10aeat.domain.manageArticle.repository.ManageArticleRepository;
-import com.final_10aeat.domain.repairArticle.entity.RepairArticle;
-import com.final_10aeat.domain.repairArticle.repository.RepairArticleRepository;
-import jakarta.annotation.PostConstruct;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
