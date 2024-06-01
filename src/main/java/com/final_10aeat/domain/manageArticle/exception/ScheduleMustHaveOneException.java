505d0185694d2e5664475e67bb012fe9618612ea
Galmaeki <kjh09800@gmail.com>
2024-06-01 14:08:34 +0900

    [fix] 잘못된 에러코드 변경
    변경 사항 :
    - ScheduleMustHaveOneException : 올바른 에러코드로 변경

--- src/main/java/com/final_10aeat/domain/manageArticle/exception/ScheduleMustHaveOneException.java
+++ src/main/java/com/final_10aeat/domain/manageArticle/exception/ScheduleMustHaveOneException.java
@@ -5,7 +5,7 @@ import com.final_10aeat.global.exception.ErrorCode;
 
 public class ScheduleMustHaveOneException extends ApplicationException {
 
-    private static final ErrorCode ERROR_CODE = ErrorCode.SCHEDULE_NOT_FOUND;
+    private static final ErrorCode ERROR_CODE = ErrorCode.SCHEDULE_MUST_HAVE_ONE;
 
     public ScheduleMustHaveOneException() {
 
