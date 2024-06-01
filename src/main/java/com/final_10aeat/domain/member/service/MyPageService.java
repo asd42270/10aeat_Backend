e7ffcd16f9f64a3a1b1bb59ea064a1b1894961e5
hym <ghrltjdtprbs@gmail.com>
2024-06-01 18:16:00 +0900

    [feat] 내 게시글 저장 조회 구현
    변경 사항 :
     - 내 게시글 저장 조회 기능을 구현하였습니다.
     - 모든 컨트롤러의 ResponseDTO를 ResponseEntity로 감쌌습니다.
     - 중복되는 메서드 제거

--- src/main/java/com/final_10aeat/domain/member/service/MyPageService.java
+++ src/main/java/com/final_10aeat/domain/member/service/MyPageService.java
@@ -5,6 +5,7 @@ import com.final_10aeat.domain.member.dto.request.BuildingInfoRequestDto;
 import com.final_10aeat.domain.member.dto.response.MyBuildingInfoResponseDto;
 import com.final_10aeat.domain.member.dto.response.MyCommentsResponseDto;
 import com.final_10aeat.domain.member.dto.response.MyInfoResponseDto;
+import com.final_10aeat.domain.member.dto.response.MySaveResponseDto;
 import com.final_10aeat.domain.member.entity.BuildingInfo;
 import com.final_10aeat.domain.member.entity.Member;
 import com.final_10aeat.domain.member.exception.BuildingInfoNotAssociatedException;
@@ -17,6 +18,7 @@ import com.final_10aeat.domain.member.repository.MemberRepository;
 import com.final_10aeat.domain.office.entity.Office;
 import com.final_10aeat.domain.office.exception.OfficeNotFoundException;
 import com.final_10aeat.domain.office.repository.OfficeRepository;
+import com.final_10aeat.domain.save.repository.ArticleSaveRepository;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
@@ -34,6 +36,7 @@ public class MyPageService {
     private final OfficeRepository officeRepository;
     private final BuildingInfoRepository buildingInfoRepository;
     private final CommentRepository commentRepository;
+    private final ArticleSaveRepository articleSaveRepository;
 
     public List<MyBuildingInfoResponseDto> getBuildingInfo(Member member) {
         Member loadedMember = findMemberByIdWithBuildingInfos(member.getId());
@@ -68,7 +71,7 @@ public class MyPageService {
         Office office = officeRepository.findById(defaultOfficeId)
             .orElseThrow(OfficeNotFoundException::new);
 
-        Member managedMember = findMemberById(member.getId());
+        Member managedMember = findMemberByIdWithBuildingInfos(member.getId());
 
         Set<BuildingInfo> buildingInfos = managedMember.getBuildingInfos();
         if (buildingInfos.stream().anyMatch(
@@ -93,7 +96,7 @@ public class MyPageService {
 
     @Transactional
     public void removeBuildingInfo(Member member, Long buildingInfoId) {
-        Member managedMember = findMemberById(member.getId());
+        Member managedMember = findMemberByIdWithBuildingInfos(member.getId());
 
         Set<BuildingInfo> buildingInfos = managedMember.getBuildingInfos();
         if (buildingInfos.size() <= 1) {
@@ -125,9 +128,19 @@ public class MyPageService {
             .collect(Collectors.toList());
     }
 
-    private Member findMemberById(Long memberId) {
-        return memberRepository.findById(memberId)
-            .orElseThrow(UserNotExistException::new);
+    public List<MySaveResponseDto> getMySavedArticles(Long memberId) {
+        Member member = findMemberByIdWithBuildingInfos(memberId);
+        Long defaultOfficeId = member.getDefaultOffice();
+
+        return articleSaveRepository.findByMemberAndOffice(member, defaultOfficeId)
+            .stream()
+            .map(articleSave -> new MySaveResponseDto(
+                articleSave.getRepairArticle().getId(),
+                articleSave.getRepairArticle().getTitle(),
+                articleSave.getRepairArticle().getCreatedAt(),
+                articleSave.getRepairArticle().getManager().getName()
+            ))
+            .collect(Collectors.toList());
     }
 
     private Member findMemberByIdWithBuildingInfos(Long memberId) {
