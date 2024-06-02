5849355a32a30a654b11a851fe0b44e5ed49ef0e
Galmaeki <kjh09800@gmail.com>
2024-06-03 01:10:52 +0900

    [refactor] 이슈 생성시, 게시글에도 조회하도록 수정
    변경 사항 :
    - ArticleIssueService : 생성로직 추가
    - ManageArticle : 새 필드 추가

--- src/main/java/com/final_10aeat/domain/articleIssue/service/ArticleIssueService.java
+++ src/main/java/com/final_10aeat/domain/articleIssue/service/ArticleIssueService.java
@@ -33,6 +33,8 @@ public class ArticleIssueService {
         ArticleIssue articleIssue = new ArticleIssue(request.title(), request.content(),
                 manageArticle, LocalDateTime.now(), manager);
 
+        manageArticle.setIssue(articleIssue);
+
         articleIssueRepository.save(articleIssue);
     }
 
