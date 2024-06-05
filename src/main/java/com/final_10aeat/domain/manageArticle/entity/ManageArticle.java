c05a828b2cdc2c620acf7df3deec79d61c14aa5a
hym <ghrltjdtprbs@gmail.com>
2024-06-05 16:59:09 +0900

    [refactor] ManageArticle엔터티의 Issue HashSet으로 초기화
    변경 사항 :
     - ManageArticle엔터티의 Issue를 HashSet으로 초기화하였습니다

--- src/main/java/com/final_10aeat/domain/manageArticle/entity/ManageArticle.java
+++ src/main/java/com/final_10aeat/domain/manageArticle/entity/ManageArticle.java
@@ -19,6 +19,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -85,7 +86,8 @@ public class ManageArticle extends SoftDeletableBaseTimeEntity {
     @Setter
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "article_issue_id", referencedColumnName = "id")
-    private Set<ArticleIssue> issues;
+    private Set<ArticleIssue> issues = new HashSet<>();
+    ;
 
     public void delete(LocalDateTime currentTime) {
         super.delete(currentTime);
