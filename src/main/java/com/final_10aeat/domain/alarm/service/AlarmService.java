bb93e5100a0c9fbf1171745ea92938586042db87
이유빈 <lyb991113@gmail.com>
2024-06-05 19:16:15 +0900

    [feat] 알림 기능을 구현한다. - Event Listener를 구성한다. - Event Listener(이벤트를 전달 받는 곳): 알림을 보내는 곳 - ConcurrentHashMap:

---
+++ src/main/java/com/final_10aeat/domain/alarm/service/AlarmService.java
@@ -0,0 +1,140 @@
+package com.final_10aeat.domain.alarm.service;
+
+import com.final_10aeat.common.dto.UserIdAndRole;
+import com.final_10aeat.domain.alarm.dto.response.AlarmResponseDto;
+import com.final_10aeat.domain.alarm.entity.Alarm;
+import com.final_10aeat.domain.alarm.entity.enumtype.AlarmType;
+import com.final_10aeat.domain.alarm.event.BookmarkCommentEvent;
+import com.final_10aeat.domain.alarm.event.CommentEvent;
+import com.final_10aeat.domain.alarm.exception.AlarmNotFoundException;
+import com.final_10aeat.domain.alarm.repository.AlarmRepository;
+import com.final_10aeat.domain.member.entity.Member;
+import com.final_10aeat.domain.member.exception.UserNotExistException;
+import com.final_10aeat.domain.member.repository.MemberRepository;
+import com.final_10aeat.global.util.ResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AlarmService {
+
+    private final AlarmRepository alarmRepository;
+    private final MemberRepository memberRepository;
+    private final ConcurrentHashMap<Long, DeferredResult<ResponseEntity<ResponseDTO<AlarmResponseDto>>>> alarmMap = new ConcurrentHashMap<>();
+
+    public DeferredResult<ResponseEntity<ResponseDTO<AlarmResponseDto>>> getDeferredResult(Long memberId) {
+        DeferredResult<ResponseEntity<ResponseDTO<AlarmResponseDto>>> deferredResult = new DeferredResult<>(5000L * 3);
+        deferredResult.onTimeout(
+                () -> deferredResult.setErrorResult(ResponseEntity.status(HttpStatus.NO_CONTENT)
+                        .body(ResponseDTO.ok())));
+
+        deferredResult.onCompletion(
+                () -> alarmMap.remove(memberId)
+        );
+
+        alarmMap.put(memberId, deferredResult);
+        return deferredResult;
+    }
+
+    @EventListener
+    public void commentEventHandler(CommentEvent event) {
+        Member member = event.member();
+        DeferredResult<ResponseEntity<ResponseDTO<AlarmResponseDto>>> deferredResult = alarmMap.get(member.getId());
+
+        //알림 저장 로직
+        Alarm alarm = Alarm.builder()
+                .targetId(event.commentId())
+                .alarmType(AlarmType.SAVED)
+                .member(member)
+                .build();
+
+        Alarm savedAlarm = alarmRepository.save(alarm);
+
+        if (deferredResult == null) {
+            return;
+        }
+
+        AlarmResponseDto response = AlarmResponseDto.builder()
+                .alarmId(savedAlarm.getId()) //저장된 알림의 id
+                .targetId(event.commentId())
+                .alarmType(AlarmType.ANSWER)
+                .checked(savedAlarm.getChecked())
+                .build();
+
+
+        deferredResult.setResult(ResponseEntity
+                .ok(ResponseDTO.okWithData(response)));
+    }
+
+    @EventListener
+    public void bookMarkEventHandler(BookmarkCommentEvent event) {
+
+        event.members().parallelStream()
+                .forEach(member -> {
+                    DeferredResult<ResponseEntity<ResponseDTO<AlarmResponseDto>>> deferredResult = alarmMap.get(member.getId());
+
+                    // alarm 저장 로직
+                    Alarm alarm = Alarm.builder()
+                            .targetId(event.articleId())
+                            .alarmType(AlarmType.SAVED)
+                            .member(member)
+                            .build();
+
+                    Alarm savedAlarm = alarmRepository.save(alarm);
+
+
+                    if (Optional.ofNullable(deferredResult).isPresent()) {
+                        AlarmResponseDto response = AlarmResponseDto.builder()
+                                .alarmId(savedAlarm.getId()) //저장된 알림의 id
+                                .targetId(event.articleId())
+                                .alarmType(AlarmType.SAVED)
+                                .checked(savedAlarm.getChecked())
+                                .build();
+
+                        deferredResult.setResult(ResponseEntity
+                                .ok(ResponseDTO.okWithData(response)));
+                    }
+                });
+    }
+
+    @Transactional(readOnly = true)
+    public List<AlarmResponseDto> getAlarmHistory(UserIdAndRole userIdAndRole) {
+
+        //멤버 검증 로직
+        Member foundMember = memberRepository.findById(userIdAndRole.id())
+                .orElseThrow(UserNotExistException::new);
+
+        return alarmRepository.findByMember(foundMember).stream()
+                .map(alarm -> AlarmResponseDto.builder()
+                        .alarmId(alarm.getId())
+                        .alarmType(alarm.getAlarmType())
+                        .targetId(alarm.getTargetId())
+                        .checked(alarm.getChecked())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    public void checkedAlarm(Long alarmId, UserIdAndRole userIdAndRole) {
+
+        memberRepository.findById(userIdAndRole.id())
+                .orElseThrow(UserNotExistException::new);
+
+        Alarm alarm = alarmRepository.findById(alarmId)
+                .orElseThrow(AlarmNotFoundException::new);
+
+        alarm.setChecked(true);
+    }
+}
+
