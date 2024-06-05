4587bd065a6938d58eb423d690d8155efd8206b3
hym <ghrltjdtprbs@gmail.com>
2024-06-05 16:38:10 +0900

    [refactor] 이슈 서비스 코드 수정
    문제 사항 :
     - 이슈 연관관계 수정 반영 필요
    
    변경 사항 :
     - 수정된 연관 관계에 맞춰 이슈 서비스 코드를 수정하였습니다.

--- src/main/java/com/final_10aeat/domain/articleIssue/service/ArticleIssueService.java
+++ src/main/java/com/final_10aeat/domain/articleIssue/service/ArticleIssueService.java
@@ -1,11 +1,14 @@
 package com.final_10aeat.domain.articleIssue.service;
 
+import static java.util.Optional.ofNullable;
+
 import com.final_10aeat.common.dto.UserIdAndRole;
 import com.final_10aeat.common.exception.ArticleNotFoundException;
 import com.final_10aeat.common.exception.UnauthorizedAccessException;
 import com.final_10aeat.domain.articleIssue.dto.request.ArticleIssuePublishRequestDto;
 import com.final_10aeat.domain.articleIssue.dto.request.IssueUpdateRequestDto;
 import com.final_10aeat.domain.articleIssue.entity.ArticleIssue;
+import com.final_10aeat.domain.articleIssue.exception.InactiveIssueUpdateException;
 import com.final_10aeat.domain.articleIssue.exception.IssueNotFoundException;
 import com.final_10aeat.domain.articleIssue.repository.ArticleIssueRepository;
 import com.final_10aeat.domain.manageArticle.entity.ManageArticle;
@@ -13,13 +16,10 @@ import com.final_10aeat.domain.manageArticle.repository.ManageArticleRepository;
 import com.final_10aeat.domain.manager.entity.Manager;
 import com.final_10aeat.domain.repairArticle.entity.RepairArticle;
 import com.final_10aeat.domain.repairArticle.repository.RepairArticleRepository;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-
-import static java.util.Optional.ofNullable;
 
 @Service
 @Transactional
@@ -31,72 +31,71 @@ public class ArticleIssueService {
     private final RepairArticleRepository repairArticleRepository;
 
     public void manageIssuePublish(ArticleIssuePublishRequestDto request, Long id,
-                                   Manager manager) {
+        Manager manager) {
 
         ManageArticle manageArticle = manageArticleRepository.findById(id)
-                .orElseThrow(ArticleNotFoundException::new);
+            .orElseThrow(ArticleNotFoundException::new);
+
+        manageArticle.getIssues().forEach(issue -> issue.setActive(false));
 
         ArticleIssue articleIssue = new ArticleIssue(request.title(), request.content(),
-                manageArticle, LocalDateTime.now(), manager);
+            manageArticle, manager);
 
-        manageArticle.setIssue(articleIssue);
-
+        manageArticle.getIssues().add(articleIssue);
         articleIssueRepository.save(articleIssue);
     }
 
     public void repairIssuePublish(ArticleIssuePublishRequestDto request, Long id,
-                                   Manager manager) {
+        Manager manager) {
 
         RepairArticle repairArticle = repairArticleRepository.findById(id)
-                .orElseThrow(ArticleNotFoundException::new);
+            .orElseThrow(ArticleNotFoundException::new);
+
+        repairArticle.getIssues().forEach(issue -> issue.setActive(false));
 
         ArticleIssue articleIssue = new ArticleIssue(request.title(), request.content(),
-                repairArticle, LocalDateTime.now(), manager);
+            repairArticle, manager);
 
+        repairArticle.getIssues().add(articleIssue);
         articleIssueRepository.save(articleIssue);
     }
 
     public void updateIssue(IssueUpdateRequestDto request, Long issueId,
-                       UserIdAndRole userIdAndRole) {
-
+        UserIdAndRole userIdAndRole) {
         ArticleIssue articleIssue = articleIssueRepository.findById(issueId)
-                .orElseThrow(IssueNotFoundException::new);
+            .orElseThrow(IssueNotFoundException::new);
 
         validateManager(userIdAndRole, articleIssue);
+
+        if (!articleIssue.isActive()) {
+            throw new InactiveIssueUpdateException();
+        }
 
         update(request, articleIssue);
-
     }
 
-
-    //TODO:OneToOne 관계로 인해 물리삭제로 구현, 이후 연관관계 변경 및 리팩토링 진행 시 논리삭제로 다시 변경 예정
     public void deleteIssue(Long issueId, UserIdAndRole userIdAndRole) {
-//        ArticleIssue articleIssue = articleIssueRepository.findByIdAndDeletedAtIsNull(issueId)
-//                .orElseThrow(IssueNotFoundException::new);
-
         ArticleIssue articleIssue = articleIssueRepository.findById(issueId)
-                .orElseThrow(IssueNotFoundException::new);
+            .orElseThrow(IssueNotFoundException::new);
 
         validateManager(userIdAndRole, articleIssue);
 
-//        articleIssue.delete(LocalDateTime.now());
-
-        articleIssueRepository.deleteById(issueId);
+        articleIssue.delete(LocalDateTime.now());
+        articleIssueRepository.save(articleIssue);
     }
 
     private void validateManager(UserIdAndRole userIdAndRole, ArticleIssue articleIssue) {
         if (!userIdAndRole.isManager()) {
             throw new UnauthorizedAccessException();
         }
-
         if (!articleIssue.getManager().getId().equals(userIdAndRole.id())) {
             throw new UnauthorizedAccessException();
         }
     }
-    private void update(IssueUpdateRequestDto request, ArticleIssue articleIssue) {
 
+    private void update(IssueUpdateRequestDto request, ArticleIssue articleIssue) {
         ofNullable(request.title()).ifPresent(articleIssue::setTitle);
         ofNullable(request.content()).ifPresent(articleIssue::setContent);
+        articleIssueRepository.save(articleIssue);
     }
-
 }
