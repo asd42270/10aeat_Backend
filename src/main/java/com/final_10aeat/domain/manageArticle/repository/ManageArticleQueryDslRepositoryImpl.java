e763fe261383294e1b52e43a2cb4e94cdb742c18
galmaeki <kjh09800@gmail.com>
2024-06-05 18:28:12 +0900

    [refactor] 메소드 반환타입 변경
    변경 사항 :
    - ManageArticleQueryDslRepository, ManageArticleQueryDslRepositoryImpl : searchByOfficeIdAndText 메소드가 Page 반환하도록 설정

--- src/main/java/com/final_10aeat/domain/manageArticle/repository/ManageArticleQueryDslRepositoryImpl.java
+++ src/main/java/com/final_10aeat/domain/manageArticle/repository/ManageArticleQueryDslRepositoryImpl.java
@@ -2,11 +2,17 @@ package com.final_10aeat.domain.manageArticle.repository;
 
 import com.final_10aeat.domain.manageArticle.entity.ManageArticle;
 import com.final_10aeat.domain.manageArticle.entity.QManageArticle;
-import com.final_10aeat.domain.repairArticle.entity.QRepairArticle;
-import com.final_10aeat.domain.repairArticle.entity.RepairArticle;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,17 +22,33 @@ public class ManageArticleQueryDslRepositoryImpl implements ManageArticleQueryDs
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<ManageArticle> searchByOfficeIdAndText(Long userOfficeId, String search) {
+    public Page<ManageArticle> searchByOfficeIdAndText(
+        Long userOfficeId, String search, Pageable pageRequest
+    ) {
         QManageArticle manageArticle = QManageArticle.manageArticle;
 
-        return queryFactory.selectFrom(manageArticle)
+        JPAQuery<ManageArticle> query = queryFactory.selectFrom(manageArticle)
             .where(manageArticle.title.contains(search)
-                .or(manageArticle.legalBasis.contains(search))
-                .or(manageArticle.target.contains(search))
-                .or(manageArticle.responsibility.contains(search)),
+                    .or(manageArticle.legalBasis.contains(search))
+                    .or(manageArticle.target.contains(search))
+                    .or(manageArticle.responsibility.contains(search)),
                 manageArticle.office.id.eq(userOfficeId),
                 manageArticle.deletedAt.isNull()
-            ).fetch();
+            )
+            .offset(pageRequest.getOffset())
+            .limit(pageRequest.getPageSize());
+
+        for (Sort.Order order : pageRequest.getSort()) {
+            query.orderBy(
+                new OrderSpecifier(order.isAscending() ? Order.ASC : Order.DESC,
+                    new PathBuilder<>(
+                        manageArticle.getType(), manageArticle.getMetadata()
+                    ).get(order.getProperty())));
+        }
+
+        QueryResults<ManageArticle> queryResult = query.fetchResults();
+
+        return new PageImpl<>(queryResult.getResults(), pageRequest, queryResult.getTotal());
     }
 }
 
