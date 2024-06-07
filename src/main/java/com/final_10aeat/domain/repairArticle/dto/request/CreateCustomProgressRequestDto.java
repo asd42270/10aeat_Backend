3c652acf1c65e3083cd5e522ceb41f4a7512f186
hym <ghrltjdtprbs@gmail.com>
2024-06-07 20:08:50 +0900

    [refactor] 기획에 맞춰 custom progress에 endSchedule 삭제
    문제 사항 :
     - 기획에 custom progress에 endSchedule이 없음
    
    변경 사항 :
     - custom progress에 endSchedule 관련 부분 모두 제거하였습니다.

--- src/main/java/com/final_10aeat/domain/repairArticle/dto/request/CreateCustomProgressRequestDto.java
+++ src/main/java/com/final_10aeat/domain/repairArticle/dto/request/CreateCustomProgressRequestDto.java
@@ -8,7 +8,6 @@ public record CreateCustomProgressRequestDto(
 
     @NotNull(message = "시작 일정은 필수 값 입니다.")
     LocalDateTime startSchedule,
-    LocalDateTime endSchedule,
     @NotBlank(message = "제목을 입력해주세요.")
     String title,
     @NotBlank(message = "내용을 입력해주세요.")
