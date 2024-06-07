25ec8a6a9b92dc7210bd882e658a84f66afb94ac
asd42270 <lyb991113@gmail.com>
2024-06-07 17:40:06 +0900

    [test] getDeferredResult의 테스트 코드를 작성한다. - AlarmGetDeferredResultServiceTest: 객체를 잘 반환하는지, 타임아웃이 잘 발생하는지를 테스트한다.

---
+++ src/test/java/com/final_10aeat/domain/alarm/AlarmGetDeferredResultServiceTest.java
@@ -0,0 +1,67 @@
+package com.final_10aeat.domain.alarm;
+
+import com.final_10aeat.domain.alarm.dto.response.AlarmResponseDto;
+import com.final_10aeat.domain.alarm.service.AlarmService;
+import com.final_10aeat.global.util.ResponseDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.event.RecordApplicationEvents;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@RecordApplicationEvents
+public class AlarmGetDeferredResultServiceTest {
+
+    @InjectMocks
+    private AlarmService alarmService;
+
+    private final ConcurrentHashMap<Long, DeferredResult<ResponseEntity<ResponseDTO<AlarmResponseDto>>>> alarmMap = new ConcurrentHashMap<>();
+
+    @BeforeEach
+    public void setUp()  { MockitoAnnotations.openMocks(this); }
+
+    @Nested
+    @DisplayName("getDeferredResult()는")
+    class Context_getDeferredResult{
+
+        @DisplayName("DeferredResult 반환에 성공한다.")
+        @Test
+        void _willSuccess() {
+            // given
+            Long memberId = 1L;
+
+            // when&then
+            assertThat(alarmService.getDeferredResult(memberId)).isNotNull();
+        }
+
+        @DisplayName("타임 아웃이 발생한다.")
+        @Test
+        void _willTimeOut() {
+            // given
+            Long memberId = 1L;
+
+            //when
+            DeferredResult<ResponseEntity<ResponseDTO<AlarmResponseDto>>> result = alarmService.getDeferredResult(memberId);
+            result.setErrorResult(ResponseEntity.status(HttpStatus.NO_CONTENT).body(ResponseDTO.ok()));
+
+            // then
+            result.onTimeout(() -> {
+                assertTrue(result.hasResult());
+                assertEquals(HttpStatus.NO_CONTENT, ((ResponseEntity<ResponseDTO<AlarmResponseDto>>) Objects.requireNonNull(result.getResult())).getStatusCode());
+            });
+        }
+    }
+}
