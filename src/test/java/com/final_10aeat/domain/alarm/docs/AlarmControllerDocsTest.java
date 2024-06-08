f905969bfe25a336fa646e09243418d3bc86f372
asd42270 <lyb991113@gmail.com>
2024-06-08 23:52:50 +0900

    [test] 알림 서비스의 컨트롤러 Docs 테스트 코드를 작성한다. - 알림 히스토리, 알림 확인 여부 작성 완료

--- src/test/java/com/final_10aeat/domain/alarm/docs/AlarmControllerDocsTest.java
+++ src/test/java/com/final_10aeat/domain/alarm/docs/AlarmControllerDocsTest.java
@@ -1,6 +1,7 @@
 package com.final_10aeat.domain.alarm.docs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.final_10aeat.common.dto.UserIdAndRole;
 import com.final_10aeat.common.service.AuthenticationService;
 import com.final_10aeat.common.util.member.WithMember;
 import com.final_10aeat.docs.RestDocsSupport;
@@ -14,17 +15,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.request.async.DeferredResult;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -106,8 +113,63 @@ public class AlarmControllerDocsTest extends RestDocsSupport {
                                 fieldWithPath("code").description("응답상태 데이터")
                         )
                 ));
-
     }
 
+    @DisplayName("알림 히스토리 조회 API 문서화")
+    @Test
+    @WithMember
+    void _testGetAlarmHistory() throws Exception {
+        // given
+        AlarmResponseDto responseDto1 = new AlarmResponseDto(1L, AlarmType.ANSWER, 1L, false);
+        AlarmResponseDto responseDto2 = new AlarmResponseDto(2L, AlarmType.SAVED, 1L, true);
 
+        List<AlarmResponseDto> response = List.of(responseDto1, responseDto2);
+
+        UserIdAndRole userIdAndRole = new UserIdAndRole(1L, false);
+
+        // when
+        when(authenticationService.getCurrentUserIdAndRole()).thenReturn(userIdAndRole);
+        when(alarmService.getAlarmHistory(any(UserIdAndRole.class))).thenReturn(response);
+
+        // then
+        mockMvc.perform(get("/alarm/history")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("alarm-history",
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("code").description("응답상태 코드"),
+                                fieldWithPath("data[].alarmId").description("알림 id"),
+                                fieldWithPath("data[].alarmType").description("알림 유형"),
+                                fieldWithPath("data[].targetId").description("알림 내용 id"),
+                                fieldWithPath("data[].checked").description("확인 여부")
+                        )
+                ));
+    }
+
+    @DisplayName("알림 확인 API 문서화")
+    @Test
+    @WithMember
+    void _checkedAlarm() throws Exception {
+        // given
+        UserIdAndRole userIdAndRole = new UserIdAndRole(1L, false);
+
+        // when
+        when(authenticationService.getCurrentUserIdAndRole()).thenReturn(userIdAndRole);
+        doNothing().when(alarmService).checkedAlarm(any(Long.class), any(UserIdAndRole.class));
+
+        // then
+        mockMvc.perform(post("/alarm/{alarm_id}", 1L))
+                .andExpect(status().isOk())
+                .andDo(document("alarm-check",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("alarm_id").description("알림 id")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").description("응답 상태 코드")
+                        )
+                ));
+    }
 }
