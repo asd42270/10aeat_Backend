ef7c5c2276632347438c4e3dd673e972fc6a0d93
galmaeki <kjh09800@gmail.com>
2024-06-10 15:51:50 +0900

    [refactor] 사용되지 않는 import문 삭제
    변경사항 :
    S3Config : 사용되지 않는 import 삭제

--- src/main/java/com/final_10aeat/global/config/S3Config.java
+++ src/main/java/com/final_10aeat/global/config/S3Config.java
@@ -1,14 +1,10 @@
 package com.final_10aeat.global.config;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.Bucket;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
