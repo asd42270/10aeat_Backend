66bb9bb9725f1be4066f14aa941de5178f10c998
hym <ghrltjdtprbs@gmail.com>
2024-06-12 20:04:33 +0900

    [refactor] 유지보수 게시글 수정 dto에 이미지 제거
    문제 사항 :
     - 이미지 등록 방식이 변경되어 유지보수 게시글에 이미지 수정 부분이 제거되어야함
    
    변경 사항 :
    - UpdateRepairArticleRequestDto 에 images 제거

--- src/main/java/com/final_10aeat/domain/repairArticle/dto/request/UpdateRepairArticleRequestDto.java
+++ src/main/java/com/final_10aeat/domain/repairArticle/dto/request/UpdateRepairArticleRequestDto.java
@@ -3,7 +3,6 @@ package com.final_10aeat.domain.repairArticle.dto.request;
 import com.final_10aeat.common.enumclass.ArticleCategory;
 import com.final_10aeat.common.enumclass.Progress;
 import java.time.LocalDateTime;
-import java.util.List;
 
 public record UpdateRepairArticleRequestDto(
     ArticleCategory category,
@@ -13,8 +12,7 @@ public record UpdateRepairArticleRequestDto(
     LocalDateTime constructionStart,
     LocalDateTime constructionEnd,
     String repairCompany,
-    String repairCompanyWebsite,
-    List<String> images
+    String repairCompanyWebsite
 ) {
 
 }
