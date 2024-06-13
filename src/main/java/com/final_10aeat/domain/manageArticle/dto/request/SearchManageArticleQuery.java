9819a2ef40b3398ab032285488f25c62a61df7cb
galmaeki <kjh09800@gmail.com>
2024-06-13 11:48:53 +0900

    [refactor] 메소드 생성자 builder로 변경
    변경 사항 :
    - SearchManageArticleQuery

--- src/main/java/com/final_10aeat/domain/manageArticle/dto/request/SearchManageArticleQuery.java
+++ src/main/java/com/final_10aeat/domain/manageArticle/dto/request/SearchManageArticleQuery.java
@@ -1,8 +1,10 @@
 package com.final_10aeat.domain.manageArticle.dto.request;
 
 import java.time.LocalDateTime;
+import lombok.Builder;
 import org.springframework.data.domain.Pageable;
 
+@Builder
 public record SearchManageArticleQuery(
     LocalDateTime now,
     String keyword,
@@ -12,17 +14,4 @@ public record SearchManageArticleQuery(
     Pageable pageRequest
 ) {
 
-    public static SearchManageArticleQuery toSearchQuery(
-        LocalDateTime now, String keyword, Integer year,
-        Integer month, Long officeId, Pageable pageRequest
-    ) {
-        return new SearchManageArticleQuery(
-            now,
-            keyword,
-            year,
-            month,
-            officeId,
-            pageRequest
-        );
-    }
 }
